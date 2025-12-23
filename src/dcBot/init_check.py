40bfcde225dbdabbfc41d792d1144916441acdf1
JareCoder <jesse.m.lappalainen@gmail.com>
2025-12-24 00:46:02 +0200

    Fix: Order of config

--- src/dcBot/init_check.py
+++ src/dcBot/init_check.py
@@ -5,7 +5,7 @@ from dcBot.data_handler import BOT_DATA_FILE, save_bot_data
 def ensure_bot_data_json_exists() -> None:
     if not os.path.exists(BOT_DATA_FILE):
         print(f"📝 Creating {BOT_DATA_FILE}...")
-        default_data = {"players": [], "botConfig": {}}
+        default_data = {"botConfig": {},"players": []}
         save_bot_data(default_data)
         print(f"✅ Created {BOT_DATA_FILE} with default structure.")
     else:
